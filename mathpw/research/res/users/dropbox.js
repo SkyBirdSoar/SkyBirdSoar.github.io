04b37da515fb8253362df6e785cdcc5488125cb6
SkyBirdSoar <spfdisposable-github@yahoo.com.sg>
2014-07-10 18:32:59 +0800

    i like this update very much

---
+++ mathpw/research/res/users/dropbox.js
@@ -0,0 +1,29 @@
+var urlParams;
+(window.onpopstate = function () {
+  var match,
+      pl     = /\+/g,  // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+      query  = window.location.search.substring(1);
+
+  urlParams = {};
+  while (match = search.exec(query))
+     urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+if (urlParams["token"] == null) {
+  alert("Error: No token");
+} else {
+  var client = new Dropbox.Client({key: "xjnqbt5axzmwl9e", token: urlParams["token"]});
+
+  // Try to finish OAuth authorization.
+  client.authenticate({interactive: false}, function (error) {
+    if (error) {
+      alert('Dropbox Authentication error: ' + error);
+    }
+  });
+}
+
+$(document).ready(function() {
+  
+});
