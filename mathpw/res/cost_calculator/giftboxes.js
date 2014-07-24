e6f671e21e67a6ee3283f1f1591d9254a2e6d8c7
SkyBirdSoar <spfdisposable-github@yahoo.com.sg>
2014-07-24 14:46:55 +0000

    added boxes to the list

--- mathpw/res/cost_calculator/giftboxes.js
+++ mathpw/res/cost_calculator/giftboxes.js
@@ -29,10 +29,10 @@ var Heart = function(radius, height, lidHeight) {
       var perimeterOfLid = (2 * pi * lidRadius) + 4 * lidRadius;
       var breadthOfHeart = 2 * radius + Math.sqrt(radius.square() + radius.square());
       var lengthOfHeart = 2 * Math.sqrt(0.5 * (2 * radius).square()) + (radius - (radius * Math.cos(toRadians(45))));
-      var areaUsedByHeartBase = breadthOfHeart * lengthOfHeart;
+      var areaOfPaperUsedByHeartBase = breadthOfHeart * lengthOfHeart;
       var breadthOfLid = 2 * lidRadius + Math.sqrt(lidRadius.square() + lidRadius.square());
       var lengthOfLid = 2 * Math.sqrt(0.5 * (2 * lidRadius).square()) + (lidRadius - (lidRadius * Math.cos(toRadians(45))));
-      var areaUsedByLid = breadthOfLid * lengthOfLid;
+      var areaOfPaperUsedByLid = breadthOfLid * lengthOfLid;
       var lateralAreaOfBase = perimeterOfHeartBase * height;
       var lateralAreaOfLid = perimeterOfLid * lidHeight;
       
@@ -44,15 +44,52 @@ var Heart = function(radius, height, lidHeight) {
         perimeterOfLid: perimeterOfLid,
         breadthOfHeart: breadthOfHeart,
         lengthOfHeart: lengthOfHeart,
-        areaUsedByHeartBase: areaUsedByHeartBase,
+        areaOfPaperUsedByHeartBase: areaOfPaperUsedByHeartBase,
         breadthOfLid: breadthOfLid,
         lengthOfLid: lengthOfLid,
-        areaUsedByLid: areaUsedByLid,
+        areaOfPaperUsedByLid: areaOfPaperUsedByLid,
         lateralAreaOfBase: lateralAreaOfBase,
         lateralAreaOfLid: lateralAreaOfLid
       };
     }
-    
     return properties;
   };
+  
+  this.calculateProperties();
+};
+
+var Box = function(length, breadth, height, lidHeight) {
+  this.length = length;
+  this.breadth = breadth;
+  this.height = height;
+  this.lidHeight = lidHeight;
+  
+  var properties = null;
+  
+  this.calculateProperties = function() {
+    if (properties === null) {
+      var baseArea = length * breadth;
+      var baseAreaOfLid = (length + 0.2) * (breadth + 0.2);
+      var latA = length * height;
+      var latB = breadth * height;
+      var lateralArea = (2 * latA) + (2 * latB);
+      var latALid = (length + 0.2) * (lidHeight + 0.2);
+      var latBLid = (breadth + 0.2) * (lidHeight + 0.2);
+      var lateralAreaOfLid = (2 * latALid) + (2 * latBLid);
+      
+      properties = {
+        baseArea: baseArea,
+        baseAreaOfLid: baseAreaOfLid,
+        latA: latA,
+        latB: latB,
+        lateralArea: lateralArea,
+        latALid: latALid,
+        latBLid: latBLid,
+        lateralAreaOfLid: lateralAreaOfLid
+      };
+    }
+    return properties;
+  };
+  
+  this.calculateProperties();
 };
